8fac620d75cb54a5e0625806b8787b5d3fcec98a
Gabriel Martins Fortunato de Paula <gabrielmartins180706@gmail.com>
2025-06-15 14:05:38 -0300

    Carousel terminado, mudanças em index e rodapé e  pastas CSS e JS adicionadas com bootstrap

---
+++ carousel.php
@@ -0,0 +1,26 @@
+<div id="banners" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#banners" data-slide-to="0" class="active"></li>
+        <li data-target="#banners" data-slide-to="1" class="active"></li>
+        <li data-target="#banners" data-slide-to="2" class="active"></li>
+    </ol>
+    <div class="carousel-iner" role="listbox">
+        <div class="item active">
+            <img src="img/banner1.jpg" alt="Banner 1">
+        </div>
+        <div class="item active">
+            <img src="img/banner2.jpg" alt="Banner 1">
+        </div>
+        <div class="item active">
+            <img src="img/banner3.jpg" alt="Banner 1">
+        </div>
+    </div>
+    <a href="#banners" class="left carousel-control" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hiden="true"></span>
+        <span class="sr-only">Anterior</span>
+    </a>
+    <a href="#banners" class="right carousel-control" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Próximo</span>
+    </a>
+</div>
